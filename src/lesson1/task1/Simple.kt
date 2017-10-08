a53efc625c8452a22d29dbbb780f7176db571db2
MayneKeen <menpet2000@gmail.com>
2017-10-08 15:36:18 +0300

    120 символов

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -93,7 +93,8 @@ fun thirdDigit(number: Int): Int = (number%1000)/100
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = ((hoursArrive*60+minutesArrive) - (hoursDepart*60+minutesDepart))
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        ((hoursArrive*60+minutesArrive) - (hoursDepart*60+minutesDepart))
 
 /**
  * Простая
@@ -102,7 +103,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1.0 + percent*0.01)*(1.0 + percent*0.01)*(1.0 + percent*0.01)
+fun accountInThreeYears(initial: Int, percent: Int): Double =
+        initial * (1.0 + percent*0.01)*(1.0 + percent*0.01)*(1.0 + percent*0.01)
 /**
  * Простая
  *
