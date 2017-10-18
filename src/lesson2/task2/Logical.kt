836ae1f04fa220b9531b3ca4d0d2c32bed36c2f7
MayneKeen <menpet2000@gmail.com>
2017-10-18 18:24:14 +0300

    -_-

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -29,7 +29,7 @@ fun isNumberHappy(number: Int): Boolean = (number/1000 + (number/100)%10) == (nu
  * Считать, что ферзи не могут загораживать друг друга.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
-        (((x1==x2) || (y1==y2)) || ((x1 - x2)==(y1 - y2)) || ((x1 - x2)==(y2 - y1)))
+        ((x1==x2) || (y1==y2)) || ((x1 - x2)==(y1 - y2)) || ((x1 - x2)==(y2 - y1))
 
 
 /**
@@ -43,7 +43,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
 
 fun circleInside(x1: Double, y1: Double, r1: Double,
                  x2: Double, y2: Double, r2: Double): Boolean =
-        (((sqr(x1 - x2) + sqr(y1 - y2)) <= sqr(r1 - r2)) && (r1 <= r2))
+       ((sqr(x1 - x2) + sqr(y1 - y2)) <= sqr(r1 - r2)) && (r1 <= r2)
 
 /**
  * Средняя
