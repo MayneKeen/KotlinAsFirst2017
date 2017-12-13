7d54debc35816642acd52341a86e19e7e623ab29
MayneKeen <menpet2000@gmail.com>
2017-12-13 06:23:48 +0300

    task 1 from the 7th lesson is kinda done

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -38,32 +38,49 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = MatrixImpl<E>(height, width, e)
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E): Matrix<E> {
 
-    override val width: Int = TODO()
+    private val list = MutableList(height*width, {e})
 
-    override fun get(row: Int, column: Int): E  = TODO()
-
-    override fun get(cell: Cell): E  = TODO()
-
-    override fun set(row: Int, column: Int, value: E) {
-        TODO()
+    init {
+        if(height<1 || width<1) throw IllegalArgumentException()
     }
 
-    override fun set(cell: Cell, value: E) {
-        TODO()
+    override fun get(row: Int, column: Int): E  = if(row in 0 until height && column in 0 until width)
+        list[row*width + column] else throw IllegalArgumentException()
+
+    override fun get(cell: Cell): E  = get(cell.row, cell.column)
+
+    override fun set(row: Int, column: Int, value: E) = if(row in 0 until height && column in 0 until width)
+        list[row*width + column] = value else throw IllegalArgumentException()
+
+    override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
+
+    override fun equals(other: Any?) = other is MatrixImpl<*> && height==other.height &&
+            width==other.width && list==other.list
+
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        for (row in 0 until height) {
+            sb.append("[")
+            for (column in 0 until width) {
+                sb.append(this[row, column])
+            }
+            sb.append("]")
+        }
+        sb.append("]")
+        return "$sb"
     }
 
-    override fun equals(other: Any?) = TODO()
-
-    override fun toString(): String = TODO()
+    override fun hashCode(): Int = list.hashCode()
 }
 
